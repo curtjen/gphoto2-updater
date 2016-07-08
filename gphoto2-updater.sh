cb79efefe84b07a9bacbefa0cd5155648d62a15d
Curtis Jensen <jensen.cletus@gmail.com>
2016-07-07 22:41:32 -0600

    Update gphoto2-updater.sh
    Updated script to install gphoto2 version 2.5.9 rather than 2.5.10 due to conflicts with Canon T5i camera capturing video and photos.

--- gphoto2-updater.sh
+++ gphoto2-updater.sh
@@ -31,7 +31,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-latest_stable_version=2_5_10
+latest_stable_version=2_5_9
 display_version=$(echo ${latest_stable_version} | tr '_' '.')
 branch_libgphoto=''
 branch_gphoto=''
